69d8e9c40fd87e2de590bd4bae91e2fbe9ed8980
Ryan King <ryan.king1809@gmail.com>
2020-02-17 19:53:06 -0500

    make sure to return values

--- addDurationToDate.js
+++ addDurationToDate.js
@@ -26,8 +26,39 @@ export const addDurationToDate = (date, duration) => {
 	return date;
 }
 
+export const subDurationFromDate = (date, duration) => {
+	date = cloneDate(date);
+	duration = standardizeDuration(duration);
+
+	const months = duration.years * 12 + duration.months;
+	const partialMonths = months % 1;
+	const days = duration.weeks * 7 + duration.days;
+	const partialDays = days % 1;
+
+	// set date, month, year will always round to lowest integer
+	// so we need to add the milliseconds for partial months
+	date.setMonth(date.getMonth() - duration.years * 12 - duration.months);
+	date.setDate(date.getDate() - duration.weeks * 7 - duration.days);
+	date.setHours(
+		date.getHours() - duration.hours,
+		date.getMinutes() - duration.minutes,
+		date.getSeconds() - duration.seconds,
+		date.getMilliseconds() -
+			duration.milliseconds -
+			partialDays * unitMs["days"] -
+			partialMonths * unitMs["months"]
+	);
+	return date;
+};
+
 export const addUnitToDate = (date, unit, value) => {
 	let duration = {};
 	duration[unit] = value;
-	addDurationToDate(date, duration)
+	return addDurationToDate(date, duration)
 }
+
+export const subUnitFromDate = (date, unit, value) => {
+	let duration = {};
+	duration[unit] = -value;
+	return addDurationToDate(date, duration);
+};
