fe9cdf9e4b34be248d45f19a6df37678926c6b5c
Ryan King <ryan.king1809@gmail.com>
2020-05-06 11:36:44 -0500

    ya ding-dong

--- src/matchDurationUnits.js
+++ src/matchDurationUnits.js
@@ -2,11 +2,11 @@ import { durationToUnit } from "./durationToUnit";
 import { simplifyDuration } from "./simplifyDuration";
 
 export const matchDurationUnits = (duration1, duration2) => {
-    duration1 = simplifyDuration(rate1);
-    duration2 = simplifyDuration(rate2);
+    duration1 = simplifyDuration(duration1)
+    duration2 = simplifyDuration(duration2)
     duration1Unit = Object.keys(duration1)[0]
-    duration2Unit = Object.keys(duration1)[0]
-    duration2Divider = durationToUnit(duration1, rate1Unit)
+    duration2Unit = Object.keys(duration2)[0]
+    duration2Divider = durationToUnit(duration2, duration1Unit)
     let newDuration2 = {}
     newDuration2[duration1Unit] = duration2[duration2Unit] / duration2Divider
     return [duration1, newDuration2]
